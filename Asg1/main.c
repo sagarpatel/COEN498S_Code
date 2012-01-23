8496c1a549c87a55bab5028d0fb9bedd6423aa0d
Sagar Patel <17.sagar.patel@gmail.com>
2012-01-23 00:44:29 -0500

    Now using correct functions and structures. Need to start saving to file now.

--- Asg1/main.c
+++ Asg1/main.c
@@ -3,27 +3,73 @@
 
 #define SIZE 256
 
-int
-main (void)
+
+
+int main (void)
 {
-  char buffer[SIZE];
-  time_t curtime;
-  struct tm *loctime;
+  /*
+  time_t now;
+  struct tm ts;
+  char buf[80];
 
-  /* Get the current time.  */
-  curtime = time (NULL);
+  //Get current time
+  time(&now);
 
-  /* Convert it to local time representation.  */
-  loctime = localtime (&curtime);
+  //Format time
+  ts = *localtime(&now);
+  strftime(buf, sizeof(buf),"%a %Y-%m-%d %H:%M:%S %Z", &ts);
+  printf ("%s\n", buf);
+*/
 
-  /* Print out the date and time in the standard format.  */
-  fputs (asctime (loctime), stdout);
+/*
+  struct tm t;
+  time_t t_of_day;
+  t.tm_year = 2011-1900;
+  t.tm_mon = 7;           // Month, 0 - jan
+  t.tm_mday = 8;          // Day of the month
+  t.tm_hour = 16;     
+  t.tm_min = 11;
+  t.tm_sec = 42;
+  t.tm_isdst = -1;        // Is DST on? 1 = yes, 0 = no, -1 = unknown
+  t_of_day = mktime(&t);
+  printf("seconds since the Epoch: %ld\n", (long) t_of_day);
 
-  /* Print it out in a nice format.  */
-  strftime (buffer, SIZE, "Today is %A, %B %d.\n", loctime);
-  fputs (buffer, stdout);
-  strftime (buffer, SIZE, "The time is %I:%M %p.\n", loctime);
-  fputs (buffer, stdout);
+*/
+
+
+  printf("\n Enter time and date in following format : YYYY MM DD HOUR MIN SEC \n");
+
+  struct tm userTimeInput;
+  time_t secondFromTime;
+
+  int year;
+  int month;
+
+  char buf[100];
+  fgets(buf, sizeof(buf), stdin);
+
+  int actualYear;
+
+  sscanf(buf, "%d %d %d %d %d %d", &actualYear, 
+                                &userTimeInput.tm_mon,
+                                &userTimeInput.tm_mday,
+                                &userTimeInput.tm_hour,
+                                &userTimeInput.tm_min,
+                                &userTimeInput.tm_sec);
+
+  userTimeInput.tm_year = actualYear - 1900;
+
+
+
+  userTimeInput.tm_isdst = -1; 
+
+  secondFromTime = mktime(&userTimeInput);
+  printf("seconds between epoch set time: %ld\n", secondFromTime);
+
+  unsigned long uL = (unsigned long)secondFromTime;
+  float f = (float)uL;
+  printf("seconds between epoch set time: %ld\n", uL);
+  printf("seconds between epoch set time: %f\n", f);
 
   return 0;
 }
