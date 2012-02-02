0bc779825ddcd907304eac426203d72e040ea3bd
Sagar Patel <17.sagar.patel@gmail.com>
2012-02-02 15:08:17 -0500

    New code from scratch, testing struct, may need only milliseconds

--- Asg1/main.c
+++ Asg1/main.c
@@ -1,29 +1,52 @@
-#include <time.h>
+#include <sys/time.h>
 #include <stdio.h>
 
 #define SIZE 256
 
-int
-main (void)
+int main (void)
 {
-  char buffer[SIZE];
-  time_t curtime;
-  struct tm *loctime;
 
-  /* Get the current time.  */
-  curtime = time (NULL);
 
-  /* Convert it to local time representation.  */
-  loctime = localtime (&curtime);
 
-  /* Print out the date and time in the standard format.  */
-  fputs (asctime (loctime), stdout);
+  struct timeval oldTime;
+  struct timeval newTime;
+  gettimeofday(&oldTime, NULL);
+  int usecDT = 0 ;//oldTime.tv_usec;
 
-  /* Print it out in a nice format.  */
-  strftime (buffer, SIZE, "Today is %A, %B %d.\n", loctime);
-  fputs (buffer, stdout);
-  strftime (buffer, SIZE, "The time is %I:%M %p.\n", loctime);
-  fputs (buffer, stdout);
+  unsigned long long totalUsec = 0;
+  unsigned long long totalMsec = 0;
+  unsigned long long totalsec = 0;
+
+  //printf("%d",usecDT);
+
+  int counter = 0;
+
+  while(counter <1300)
+  {
+    
+
+    //get current time
+    gettimeofday(&newTime, NULL);
+
+    usecDT = newTime.tv_usec - oldTime.tv_usec;
+
+    totalUsec = (unsigned long long)usecDT;
+    totalMsec += totalUsec/1000;
+    totalsec  = totalMsec/1000;
+
+    gettimeofday(&oldTime, NULL);
+
+
+    printf("\n\n %d",totalUsec);
+    printf("\n %d", totalMsec);
+    printf("\n %d", totalsec);
+
+    counter++;
+  }
+
+
+
+
 
   return 0;
 }
