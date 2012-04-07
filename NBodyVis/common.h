7622e885594e20bc2d35df7c0136f10b9fd9e7ce
Sagar Patel <17.sagar.patel@gmail.com>
2012-04-07 01:23:06 -0400

    Tweaking common.h value. It seems that scaling G up helps, my guess is the extremely low value led to loss of data down the line. Scaling G up could also help with large mass values (planets, sun) later on

--- NBodyVis/common.h
+++ NBodyVis/common.h
@@ -4,9 +4,9 @@
 #define __common_h__
 
 #define PARTICLES_MAXCOUNT 2 //must be power of 2 in orderfor array data align to work later on
-#define PARTICLES_DEFAULTMASS 10000.0 // 1.0 is 1 kg
-#define GRAVITATIONALCONSTANT  0.00000000006673 //0.000006673 real value is 6.673 * 10^-11
-#define DELTA_TIME 60
+#define PARTICLES_DEFAULTMASS 1.0 // 1.0 is 1 kg
+#define GRAVITATIONALCONSTANT   0.000006673 //0.00000000006673 real value is 6.673 * 10^-11
+#define DELTA_TIME 6
 #define GRID_SIZE 10 // grid is a +- GRID_SIZE/2 cube
 #define EPS 0.0000001 // EPS^2 constant to avoid singularities
 #define ITERATION_COUNT 5000
